b2ac1ddc9739824052d718c7d9cbb473324b7597
pinulito\oscar.alfaro <oscar.alfaro@corporacionalisa.com>
2026-09-17 21:26:42 -0600

    Ajuste de pilotos

--- controllers/core/usuario.controller.js
+++ controllers/core/usuario.controller.js
@@ -157,7 +157,7 @@ async function updateUser(req, res) {
 // ------------------------------------------------------------
 // Obtiene usuarios filtrados por id_rol (ej. id_rol=1 para pilotos)
 // ------------------------------------------------------------
-const ROL_PILOTO_LEGACY = 1;
+const ROL_PILOTO_LEGACY = 5;
 const ROL_PILOTO_REAL = 1;
 
 async function getUsersByRol(req, res) {
